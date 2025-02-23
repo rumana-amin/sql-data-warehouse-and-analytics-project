9629107476e61986245a5bd86f51f871970ddf24
Rumana Amin <135628486+rumana-amin@users.noreply.github.com>
2025-02-23 16:46:09 +0600

    Update silver_ddl.sql

--- Scripts/silver_ddl.sql
+++ Scripts/silver_ddl.sql
@@ -13,10 +13,10 @@ IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
     cst_id					INT,
-    cst_key					NVARCHAR(50),
+    cst_key				NVARCHAR(50),
     cst_firstname			NVARCHAR(50),
     cst_lastname			NVARCHAR(50),
-    cst_cst_marital_status  NVARCHAR(50),
+    cst_marital_status                  NVARCHAR(50),
     cst_gndr				NVARCHAR(50),
     cst_create_date			DATE,
     dwh_create_date			DATETIME2 DEFAULT GETDATE()
